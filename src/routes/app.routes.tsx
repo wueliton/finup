8fe6cd73b30f273eca5878a0bb00f249510d1855
wueliton <paulo.wueliton@hotmail.com>
2025-08-28 08:08:50 -0300

    chore: adjusted deploy

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -6,38 +6,43 @@ import LoginUsername from "flows/login/username";
 import useAuthMiddleware from "middlware/auth.middleware";
 import { createBrowserRouter, redirect } from "react-router";
 
-const router = createBrowserRouter([
-  {
-    path: "app",
-    loader: useAuthMiddleware,
-    element: <Home />,
-    children: [
-      {
-        index: true,
-        element: <Dashboard />,
-      },
-      {
-        path: "wallet",
-        element: <Wallet />,
-      },
-    ],
-  },
-  {
-    path: "login",
-    Component: Login,
-    children: [
-      {
-        index: true,
-        Component: LoginUsername,
-      },
-    ],
-  },
-  {
-    path: "/",
-    loader: () => {
-      throw redirect("app");
+const router = createBrowserRouter(
+  [
+    {
+      path: "app",
+      loader: useAuthMiddleware,
+      element: <Home />,
+      children: [
+        {
+          index: true,
+          element: <Dashboard />,
+        },
+        {
+          path: "wallet",
+          element: <Wallet />,
+        },
+      ],
     },
+    {
+      path: "login",
+      Component: Login,
+      children: [
+        {
+          index: true,
+          Component: LoginUsername,
+        },
+      ],
+    },
+    {
+      path: "/",
+      loader: () => {
+        throw redirect("app");
+      },
+    },
+  ],
+  {
+    basename: "/finup",
   },
-]);
+);
 
 export { router };
