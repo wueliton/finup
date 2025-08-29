b77ef86502adee6d800f165595346462aff08577
wueliton <paulo.wueliton@hotmail.com>
2025-08-28 23:50:27 -0300

    chore: added pwa configs

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: "auto",
+    }),
+  ],
   base: "/finup/",
 });
